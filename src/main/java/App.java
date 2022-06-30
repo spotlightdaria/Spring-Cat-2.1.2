84b1b5ab0f4bd14581d53166dbb28499eb4e555e
spotlightdaria <spotlightdarya@gmail.com>
2022-07-01 00:24:27 +0300

    2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        Cat cat1 = (Cat)  applicationContext.getBean("cat");
+        Cat cat2 = (Cat)  applicationContext.getBean("cat");
+        boolean compHello = bean == bean1;
+        boolean compCat = cat1 == cat2;
+        System.out.println(compHello);
+        System.out.println(compCat);
     }
 }
